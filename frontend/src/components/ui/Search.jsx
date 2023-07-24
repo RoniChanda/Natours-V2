96ec4395941cc1f85ac1183306659361549b595d
Supratim <suprachanda97@gmail.com>
2023-07-24 23:22:15 +0530

    Change in search box

--- frontend/src/components/ui/Search.jsx
+++ frontend/src/components/ui/Search.jsx
@@ -14,21 +14,19 @@ export default function Search() {
   };
 
   return (
-    <div className="nav__search">
-      <form onSubmit={submitHandler}>
-        <button type="submit" className="nav__search-btn">
-          <svg>
-            <use xlinkHref="/img/icons.svg#icon-search"></use>
-          </svg>
-        </button>
-        <input
-          type="text"
-          placeholder="Search tours"
-          className="nav__search-input"
-          onChange={(e) => setKeyword(e.target.value)}
-          value={keyword}
-        />
-      </form>
-    </div>
+    <form className="nav__search" onSubmit={submitHandler}>
+      <button type="submit" className="nav__search-btn">
+        <svg>
+          <use xlinkHref="/img/icons.svg#icon-search"></use>
+        </svg>
+      </button>
+      <input
+        type="text"
+        placeholder="Search tours"
+        className="nav__search-input"
+        onChange={(e) => setKeyword(e.target.value)}
+        value={keyword}
+      />
+    </form>
   );
 }
