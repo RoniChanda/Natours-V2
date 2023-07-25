6c2fd3938911af7f8319f489ca7869421e289dc5
Supratim <suprachanda97@gmail.com>
2023-07-25 09:44:22 +0530

    Changes in index.html

--- frontend/src/components/ui/Meta.jsx
+++ frontend/src/components/ui/Meta.jsx
@@ -5,7 +5,7 @@ export default function Meta({ title, description, keywords }) {
     <Helmet>
       <title>{title}</title>
       <meta name="description" content={description} />
-      <meta name="keyword" content={keywords} />
+      <meta name="keywords" content={keywords} />
     </Helmet>
   );
 }
